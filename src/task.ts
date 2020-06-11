be5a98b032a715ff531441d7a8f871c937d39ab4
MeJamoLeo <reonarudo11919273@gmail.com>
2020-06-11 19:10:01 +0900

    タスクの期限による条件分岐の追加

--- src/task.ts
+++ src/task.ts
@@ -25,9 +25,9 @@ const send_manthly_task = () => {
   }
 
   for (let row_n = sheet.getLastRow() - 1; row_n >= first_row; row_n--) {
-    let room_id = sheet_data[row_n][room_id_column]
-    let task_body = sheet_data[row_n][task_column]
-    let routine_day = sheet_data[row_n][sending_date_column]
+    const room_id = sheet_data[row_n][room_id_column]
+    const task_body = sheet_data[row_n][task_column]
+    const sending_date = sheet_data[row_n][sending_date_column]
     // ルームIDがなければcontinue
     if (room_id == "") {
       continue
@@ -38,28 +38,43 @@ const send_manthly_task = () => {
       continue
     }
 
-    /*
-    // メッセージの送信日がなければcontinue
-    if (sheet_data[row_n][sending_date_column] == "") {
-      continue
-    }
-    */
-
     // 今日の日付と設定した日付が一致しなければcontinue
-    if (dd_num != routine_day) {
+    if (dd_num != sending_date) {
       continue
     }
+
+    //　「タスクの期限」の有無による条件分岐
+    const limit_days = sheet_data[row_n][limit_column]
+    const get_limit_obj = (limit_days, today) => {
+
+      if (limit_days == "") {
+        const limit_obj = {
+          deadline: "0",
+          type: "none"
+        }
+        return limit_obj
+      }
+      // タスクを追加日＋期限
+      const limit_days_unix = limit_days * (60 * 60 * 24)
+      const unix_time = Math.floor(today.getTime() / 1000) + limit_days_unix;
+      const limit_obj = {
+        deadline: unix_time.toString(),
+        type: "date"
+      }
+      return limit_obj
+    }
+
+    const limit_obj = get_limit_obj(limit_days, today)
     // 担当者のIDを取得する
-    console.log(get_member_ids_str(room_id))
-    //cw_send_task(room_id, message_body)
+    const member_ids_str = get_member_ids_str(room_id)
 
-
+    cw_send_task(room_id, task_body, limit_obj.deadline, limit_obj.type, member_ids_str)
   }
-
-
 }
 
-const cw_send_task = (room_id, text) => {
+
+
+const cw_send_task = (room_id, task_body, limit, limit_type, to_ids) => {
   const CW_API_TOKEN = PropertiesService.getScriptProperties().getProperty('CW_TOKEN');
   const options = {
     "method": "post",
@@ -67,10 +82,13 @@ const cw_send_task = (room_id, text) => {
       "X-ChatWorkToken": CW_API_TOKEN
     },
     "payload": {
-      "body": text
+      "body": task_body,
+      "limit": limit,
+      "limit_type": limit_type,
+      "to_ids": to_ids
     }
   };
-  const apiUrl = "https://api.chatwork.com/v2/rooms/" + room_id + "/messages";
+  const apiUrl = "https://api.chatwork.com/v2/rooms/" + room_id + "/tasks";
   const response = UrlFetchApp.fetch(apiUrl, options);
 };
 
@@ -97,9 +115,13 @@ const get_member_ids_str = (room_id) => {
   // メンバーの情報からchatwork_idのみを配列で返す
   const get_chatwork_ids = (jsons) => {
     let member_ids = [];
+    const my_cw_id = PropertiesService.getScriptProperties().getProperty('CW_ID')
     for (let n = 0; n < jsons.length; n++) {
       const json = jsons[n];
-      const chatwork_id = json.chatwork_id
+      const chatwork_id = json.account_id
+      if (chatwork_id == my_cw_id) {
+        continue;
+      }
       member_ids.push(chatwork_id)
     }
     return member_ids
@@ -107,7 +129,7 @@ const get_member_ids_str = (room_id) => {
 
 
   const chatwork_id_str = get_chatwork_ids(get_member_info(room_id)).join(",")
-  console.log chatwork_id_str
+  console.log(chatwork_id_str)
   return chatwork_id_str
 
 
