b5c372b13f60910346f17d1a70dd71714cbef841
MeJamoLeo <reonarudo11919273@gmail.com>
2020-06-11 19:13:17 +0900

    スタートする行を変更

--- src/message.ts
+++ src/message.ts
@@ -4,7 +4,7 @@ const send_monthly_msg = () => {
   const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheet_name)
   const sheet_data = sheet.getDataRange().getValues()
   // 行や列を配列として捉える　0スタート
-  const first_row = 2 - 1;
+  const first_row = 3 - 1;
   const room_id_column = 1 - 1;
   const message_column = 2 - 1;
   const sending_date_column = 3 - 1;
